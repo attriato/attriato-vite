6c3ad5eebda3d7a41f990d5bbb9256f90b306125
“attriato” <attriato@gmail.com>
2026-09-01 12:12:27 -0500

    Defer CSS and font loading to reduce initial render blocking

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./styles.css";
+
+const loadAppStyles = () => {
+  import("./styles.css");
+};
+
+if ("requestIdleCallback" in window) {
+  window.requestIdleCallback(loadAppStyles);
+} else {
+  setTimeout(loadAppStyles, 0);
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
