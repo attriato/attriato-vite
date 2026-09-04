20aed130bfe927a5b4a4495458ffcb8d7052c93b
“attriato” <attriato@gmail.com>
2026-09-04 13:16:55 -0500

    Partytown added

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { copyLibFiles } from "@builder.io/partytown/utils";
+
+const rootDir = dirname(fileURLToPath(import.meta.url));
+
+await copyLibFiles(resolve(rootDir, "public/~partytown"));
 
 export default defineConfig({
   plugins: [react()],
